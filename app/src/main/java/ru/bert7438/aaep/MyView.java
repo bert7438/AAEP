4f18949a663c0af20c8cc2b6261c9bc68d12a7b8
bert7438 <bert7438@gmail.com>
2019-11-05 14:30:41 +0300

    added some more init

--- app/src/main/java/ru/bert7438/aaep/MyView.java
+++ app/src/main/java/ru/bert7438/aaep/MyView.java
@@ -4,12 +4,18 @@ import android.content.Context;
 import android.view.View;
 
 public class MyView extends View {
-    public MyView(Context context){
-        super(context);
-    }
-
     //this variables is used for initializing
     boolean playing = false;
     int N = 10;
+    float[] x, y, vx, vy;
+    public MyView(Context context){
+        super(context);
+        x = new float[N];
+        y = new float[N];
+        vx = new float[N];
+        vy = new float[N];
+    }
+
+
     
 }
